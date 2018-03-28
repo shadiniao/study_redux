ee125e65aa900f128c8ae8d14adc8586609eb843
zhangyizhi <157876195@qq.com>
2018-03-29 01:00:00 +0800

    ok

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,5 +1,37 @@
 import React, {Component} from 'react';
 import {reduxForm, Field, Form} from "redux-form";
+import {connect} from "react-redux";
+
+const required = value => (value
+    ? undefined
+    : '必填')
+const maxLength = max => value => {
+    return value && value.length > max
+        ? `只能填${max}个字`
+        : undefined
+}
+
+const maxLength3 = maxLength(3)
+
+function renderField({
+    input,
+    label,
+    type,
+    meta: {
+        touched,
+        error,
+        warning
+    }
+}) {
+    return (
+        <div>
+            <label>{label}</label>
+            <div>
+                <input {...input} placeholder={label} type={type}/> {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+            </div>
+        </div>
+    )
+}
 
 class MyForm extends Component {
     _submit(value) {
@@ -10,20 +42,26 @@ class MyForm extends Component {
         const {handleSubmit, pristine, reset, submitting} = this.props
         return (
             <Form onSubmit={handleSubmit(value => this._submit(value))}>
-                <div>
-                    <label htmlFor="name">name:</label>
-                    <Field name="name" component="input" type="text"/>
-                </div>
+                <Field
+                    name="name"
+                    component={renderField}
+                    type="text"
+                    label="name:"
+                    validate={[required, maxLength3]}/>
 
-                <div>
-                    <label htmlFor="code">code:</label>
-                    <Field name="code" component="input" type="text"/>
-                </div>
+                <Field name="code" component={renderField} type="text" label="code:"/>
+
                 <button type="submit" disabled={pristine || submitting}>submit</button>
-                <button type="reset" disabled={pristine || submitting} onClick={reset}>reset</button>
+                <button type="reset" disabled={submitting} onClick={reset}>reset</button>
             </Form>
         )
     }
 }
 
-export default reduxForm({form: 'form1'})(MyForm)
+MyForm = reduxForm({form: 'form1'})(MyForm)
+export default connect(state => ({
+    initialValues: {
+        name: 'zyz',
+        code: '001'
+    }
+}),)(MyForm)
